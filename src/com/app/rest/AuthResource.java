c2532b00b8252bc2413339b5954bfebd53db26f3
Dan Kostiuk <dank216@gmail.com>
2016-10-26 23:07:59 -0400

    Adjusting pom.xml to fix build

--- src/com/app/rest/AuthResource.java
+++ src/com/app/rest/AuthResource.java
@@ -7,7 +7,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
