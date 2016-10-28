57de7c194f23bab72356c4eb2ce79a65d03a12ad
Dan Kostiuk <dank216@gmail.com>
2016-10-28 14:09:48 -0400

    Added joda DateTime to pom.xml

--- src/com/app/entity/moves/MovesData.java
+++ src/com/app/entity/moves/MovesData.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.eclipse.persistence.jpa.jpql.parser.DateTime;
+import org.joda.time.DateTime;
 
 /**
  * Model representing the MovesData entity.
